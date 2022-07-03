63356af7f56b53a02e3cadc39f7b23861b02a0bc
MRoccazzella <matias.a.re@hotmail.com>
2022-07-03 18:46:27 -0300

    Desafio NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+
+
+const NavBar = () => {
+  return (
+    <div className="navbar container-fluid">
+        <div className="contenedor-icono justify-content-center align-items-center">
+          <img className="logo" src ='/images/icono-hamburguesa.png' alt="" />
+          <div className="brand-name">
+            <p className="nombre-1">MR <span className="nombre-2">Burgers</span></p>
+          </div>
+        </div>
+        <div className="secciones d-flex justify-content-evenly">
+          <a className="link" href="/">Home</a>
+          <a className="link" href="/">Productos</a>
+          <a className="link" href="/">Contacto</a>
+        </div>
+        <div className="d-flex justify-content-center">
+          <a href="/"><img src='/images/carrito-de-compras.png' href="/" alt="" /></a>
+        </div>
+    </div>
+  )
+}   
+
+export default NavBar;
