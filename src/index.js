1aa00cea997a54a618872a804246397244eba023
Matias Roccazzella <matias.a.re@hotmail.com>
2022-09-28 14:24:59 -0300

    cambio json

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './firebase/config'
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-    <App />
-  
+    <>
+        <HashRouter/>
+            <App />
+        <HashRouter/>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
